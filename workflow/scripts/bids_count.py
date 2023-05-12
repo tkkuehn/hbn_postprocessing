c11b852c516cbc59ebe6ab35997226b8648b0676
Tristan Kuehn <tkuehn@uwo.ca>
2023-05-12 12:47:43 -0400

    Add motion outlier detection

--- workflow/scripts/bids_count.py
+++ workflow/scripts/bids_count.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import glob
 import os
+import re
 from collections.abc import Callable
 from dataclasses import dataclass
 from itertools import chain
@@ -105,10 +106,110 @@ def check_html(
     )
 
 
-def check_jobs(jobs_dir: os.PathLike[str] | str) -> None:
+def _process_job_file(file_: Path) -> dict[str, str | float]:
+    with file_.open() as file_content:
+        out_content = file_content.read()
+    subj_id = out_content.partition("participant_label ")[2][0:12]
+    size_kb = file_.stat().st_size / 1000
+    return {"name": file_.name, "p_id": subj_id, "size_kb": size_kb}
+
+
+STARTED_THRESHOLD_KB = 10
+COMPLETED_THRESHOLD_KB = 400
+
+
+def check_jobs(
+    jobs_dir: os.PathLike[str] | str,
+    out_dir: os.PathLike[str] | str,
+) -> None:
+    """Parse a dir of ".out" files to check for incomplete jobs."""
     out_files = glob_dir(jobs_dir, "*.out*")
-    for file_ in out_files:
-        with file_.open() as file_content:
-            out_content = file_content.read()
-        subj_id = out_content.partition("participant_label ")[2][0:12]
-        size_kb = file_.stat().st_size / 1000
+    file_info = [_process_job_file(file_) for file_ in out_files]
+    size_df = pd.DataFrame(
+        {
+            "file_name": [file_["name"] for file_ in file_info],
+            "p_id": [file_["p_id"] for file_ in file_info],
+            "size_kb": [file_["size_kb"] for file_ in file_info],
+        },
+    ).loc[lambda df: df["p_id"].str.startswith("NDA"), :]
+    max_size = (
+        size_df.groupby("p_id")
+        .max()
+        .reset_index()
+        .assign(
+            status=lambda df: df.size_kb.map(
+                lambda size_kb: "not started"
+                if size_kb < STARTED_THRESHOLD_KB
+                else (
+                    "partial/error"
+                    if size_kb < COMPLETED_THRESHOLD_KB
+                    else "likely complete"
+                ),
+            ),
+        )
+    )
+
+    out_path = Path(out_dir)
+    max_size.to_csv(out_path / "out-size_all.csv")
+    max_size.loc[max_size.status != "likely_complete", :].to_csv(
+        out_path / "out-size_incomp.csv",
+        sep=",",
+        index=False,
+    )
+    max_size.loc[max_size.status == "likely complete", :].to_csv(
+        out_path / "out-size_comp.csv",
+        sep=",",
+        index=False,
+    )
+
+
+CONFOUNDS_PATTERN = re.compile(
+    r"sub-(?P<subject>[a-zA-Z\d]+)_task-(?P<task>[a-zA-Z\d]+)"
+    r"(?:_run-(?P<run>\d+))?_desc-(?P<description>[a-zA-Z\d]+)"
+    r"_(?P<suffix>[a-zA-Z\d]+).tsv",
+)
+
+
+def get_framewise_displacement(
+    subj_dir: os.PathLike[str] | str,
+) -> dict[str, str | float]:
+    """Get the framewise displacement in each task for a subject."""
+    subj_path = Path(subj_dir)
+    tsvs = glob_dir(subj_path / "func", "*.tsv*")
+    sub_dict: dict[str, str | float] = {"id": subj_path.name}
+    for tsv in tsvs:
+        match = re.match(CONFOUNDS_PATTERN, tsv.name)
+        if not match:
+            raise ValueError
+        task = match.group("task")
+        run = f"_{match.group('run')}" if match.group("run") else ""
+        task_run = f"{task}{run}"
+        data = pd.read_csv(tsv, sep="\t", header=0)
+        sub_dict[task_run] = data["framewise_displacement"].tail(-1).mean()
+    return sub_dict
+
+
+def exclude_by_motion(
+    bids_dir: os.PathLike[str] | str, out_dir: os.PathLike[str] | str,
+) -> None:
+    """Find outliers by framewise displacement per task."""
+    subj_dirs = glob_dir(bids_dir, "sub*", filter_=lambda path: path.is_dir())
+    displacement_df = pd.DataFrame(
+        [get_framewise_displacement(subj_dir) for subj_dir in subj_dirs],
+    )
+    group_fds = displacement_df.iloc[:, 1:].mean(axis=0)
+    group_sds = displacement_df.iloc[:, 1:].mean(axis=0)
+    upper_lims = group_fds + (2 * group_sds)
+    for task_run in set(displacement_df.columns) - {"id"}:
+        displacement_df.assign(
+            **{
+                f"{task_run}_is_outlier": displacement_df[task_run]
+                > upper_lims[task_run],
+            },
+        )
+        displacement_df.drop(task_run, axis=1)
+    displacement_df.to_csv(
+        Path(out_dir) / "motion-outliers_all.csv",
+        sep=",",
+        index=False,
+    )
