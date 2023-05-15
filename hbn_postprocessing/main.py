9471271588291d16c9fcba81a8614c2943b375f9
Tristan Kuehn <tkuehn@uwo.ca>
2023-05-15 13:25:41 -0400

    Read a subject list file

--- hbn_postprocessing/main.py
+++ hbn_postprocessing/main.py
@@ -3,12 +3,19 @@
 from __future__ import annotations
 
 from argparse import ArgumentParser
+from os import PathLike
 from pathlib import Path
 
 from hbn_postprocessing.file_count import count_all_files
 from hbn_postprocessing.html import check_html
 from hbn_postprocessing.jobs import check_jobs
 from hbn_postprocessing.motion import exclude_by_motion
+
+
+def read_subject_list(path: PathLike[str] | str) -> list[str]:
+    """Read "sub-{label}" subject IDs from a simple file."""
+    with Path(path).open("r") as file_:
+        return file_.readlines()
 
 
 def gen_parser() -> ArgumentParser:
@@ -18,12 +25,20 @@ def gen_parser() -> ArgumentParser:
     parser.add_argument("fmriprep_dir", help="fmriprep output dir")
     parser.add_argument("jobs_dir", help="Directory with .out files")
     parser.add_argument("out_dir", help="Output directory")
+    parser.add_argument(
+        "--subject_list",
+        help=(
+            "File containing subjects to postprocess (simply one sub-{label} per line."
+        ),
+    )
     return parser
 
 
 def main() -> None:
     """Run all relevant tasks."""
     args = gen_parser().parse_args()
+
+    subjects = read_subject_list(args.subject_list) if args.subject_list else None
 
     count_all_files(args.bids_dir, args.out_dir)
     check_html(
