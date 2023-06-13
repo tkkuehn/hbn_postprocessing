fb20146085d8eaccd4c20188818bfbf827fb34e7
Tristan Kuehn <tkuehn@uwo.ca>
2023-06-13 15:54:43 -0400

    Use participant_id as index and join at the end

--- hbn_postprocessing/motion.py
+++ hbn_postprocessing/motion.py
@@ -23,7 +23,7 @@ def get_framewise_displacement(
     """Get the framewise displacement in each task for a subject."""
     subj_path = Path(subj_dir)
     tsvs = glob_dir(subj_path / "func", "*.tsv*")
-    sub_dict: dict[str, str | float] = {"id": subj_path.name}
+    sub_dict: dict[str, str | float] = {"participant_id": subj_path.name}
     for tsv in tsvs:
         match = re.match(CONFOUNDS_PATTERN, tsv.name)
         if not match:
@@ -39,16 +39,20 @@ def get_framewise_displacement(
 def exclude_by_motion(
     bids_dir: os.PathLike[str] | str,
     out_dir: os.PathLike[str] | str,
-) -> None:
+) -> pd.DataFrame:
     """Find outliers by framewise displacement per task."""
     subj_dirs = glob_dir(bids_dir, "sub*", filter_=lambda path: path.is_dir())
-    displacement_df = pd.DataFrame(
-        [get_framewise_displacement(subj_dir) for subj_dir in subj_dirs],
+    displacement_df = (
+        pd.DataFrame(
+            [get_framewise_displacement(subj_dir) for subj_dir in subj_dirs],
+        )
+        .astype({"participant_id": pd.StringDtype()})
+        .set_index("participant_id")
     )
-    group_fds = displacement_df.iloc[:, 1:].mean(axis=0)
-    group_sds = displacement_df.iloc[:, 1:].mean(axis=0)
+    group_fds = displacement_df.mean(axis=0)
+    group_sds = displacement_df.mean(axis=0)
     upper_lims = group_fds + (2 * group_sds)
-    for task_run in set(displacement_df.columns) - {"id"}:
+    for task_run in displacement_df.columns:
         displacement_df = displacement_df.assign(
             **{
                 f"{task_run}_is_outlier": displacement_df[task_run]
@@ -59,5 +63,5 @@ def exclude_by_motion(
     displacement_df.to_csv(
         Path(out_dir) / "motion-outliers_all.csv",
         sep=",",
-        index=False,
     )
+    return displacement_df
